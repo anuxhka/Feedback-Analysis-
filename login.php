e4804852044f2a90656bd512f63589376f01097b
Himja18 <97022807+Himja18@users.noreply.github.com>
2023-08-09 23:28:11 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
    $cpass = md5($_POST['cpassword']);
    $user_type = $_POST['user_type'];
 
-   $select = " SELECT * FROM student WHERE email = '$email' ";
+   $select = " SELECT * FROM student WHERE email = '$email'";
 
    $result = mysqli_query($conn, $select);
 
@@ -45,32 +45,203 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+   <meta http-equiv="Pragma" content="no-cache">
+   <meta http-equiv="Expires" content="0">
    <title>login form</title>
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="style.css">
+   <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap');
+
+*{
+   font-family: 'Poppins', sans-serif;
+   margin:0; padding:0;
+   box-sizing: border-box;
+   outline: none; border:none;
+   text-decoration: none;
+}
+
+.container{
+   min-height: 100vh;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   padding:20px;
+   padding-bottom: 60px;
+}
+
+.container .content{
+   text-align: center;
+}
+
+.container .content h3{
+   font-size: 30px;
+   color:#333;
+}
+
+.container .content h3 span{
+   background: crimson;
+   color:#fff;
+   border-radius: 5px;
+   padding:0 15px;
+}
+
+.container .content h1{
+   font-size: 50px;
+   color:#333;
+}
+
+.container .content h1 span{
+   color:crimson;
+}
+
+.container .content p{
+   font-size: 25px;
+   margin-bottom: 20px;
+}
+
+.container .content .btn{
+   display: inline-block;
+   padding:10px 30px;
+   font-size: 20px;
+   background: #333;
+   color:#fff;
+   margin:0 5px;
+   text-transform: capitalize;
+}
+
+.container .content .btn:hover{
+   background: lightskyblue;
+}
+
+.form-container{
+   min-height: 100vh;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   padding:20px;
+   padding-bottom: 60px;
+   background: #eee;
+}
+
+.form-container form{
+   padding:20px;
+   border-radius: 5px;
+   box-shadow: 0 5px 10px rgba(0,0,0,.1);
+   background: #fff;
+   text-align: center;
+   width: 500px;
+}
+
+.form-container form h3{
+   font-size: 30px;
+   text-transform: uppercase;
+   margin-bottom: 10px;
+   color:#333;
+}
+
+.form-container form input,
+.form-container form select{
+   width: 100%;
+   padding:10px 15px;
+   font-size: 17px;
+   margin:8px 0;
+   background: #eee;
+   border-radius: 5px;
+}
+
+.form-container form select option{
+   background: #fff;
+}
+
+.form-container form .form-btn{
+   background: lightblue;
+   color: rgb(51, 51, 174);
+   text-transform: capitalize;
+   font-size: 20px;
+   cursor: pointer;
+}
+
+.form-container form .form-btn:hover{
+   background: lightskyblue;
+   color: black;
+}
+
+.form-container form p{
+   margin-top: 10px;
+   font-size: 20px;
+   color:#333;
+}
+
+.form-container form p a{
+   color: rgb(51, 51, 174);
+}
+
+.form-container form .error-msg{
+   margin:10px 0;
+   display: block;
+   background: crimson;
+   color:#fff;
+   border-radius: 5px;
+   font-size: 20px;
+   padding:10px;
+}
+.form-container form .password-field {
+            display: none;
+        }
+
+.form-container form .show-password {
+            display: block;
+            margin: 10px 0;
+            font-size: 20px;
+            color: rgb(51, 51, 174);
+            cursor: pointer;
+        }
+   </style>
 
 </head>
 <body>
-   
-<div class="form-container">
 
-   <form action="" method="post">
-      <h3>login now</h3>
-      <?php
-      if(isset($error)){
-         foreach($error as $error){
-            echo '<span class="error-msg">'.$error.'</span>';
-         };
-      };
-      ?>
-      <input type="email" name="email" required placeholder="enter your email">
-   <!--   <input type="password" name="password" required placeholder="enter your password"> -->
-      <input type="submit" name="submit" value="login now" class="form-btn">
-      <p>don't have an account? <a href="register.php">register now</a></p>
-   </form>
+    <div class="form-container">
+        <form action="" method="post">
+            <h3>login now</h3>
+            <?php
+                if (isset($error)) {
+                    foreach ($error as $error) {
+                        echo '<span class="error-msg">' . $error . '</span>';
+                    };
+                };
+            ?>
+            <select name="user_type" id="userType" onchange="togglePassword()">
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+            </select>
+            <input type="email" name="email" required placeholder="enter your email">
+            <div class="password-field">
+                <input type="password" name="password" placeholder="enter your password">
+            </div>
+            <input type="submit" name="submit" value="login now" class="form-btn">
+            <p>don't have an account? <a href="register.php">register now</a></p>
+        </form>
+    </div>
 
-</div>
+    <script>
+        function togglePassword() {
+            const userType = document.getElementById("userType").value;
+            const passwordField = document.querySelector(".password-field");
 
+            if (userType === "admin") {
+                passwordField.style.display = "block";
+            } else {
+                passwordField.style.display = "none";
+            }
+        }
+        if (window.history && history.pushState) {
+    history.pushState(null, null, location.href);
+    window.onpopstate = function () {
+        history.go(1);
+    };
+   }
+    </script>
 </body>
 </html>
